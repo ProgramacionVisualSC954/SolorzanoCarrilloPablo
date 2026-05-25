acd7bd7810cdc54fe4deded5b513cd673ff8cd5c
PabloC <pscarrillo20@gmail.com>
2026-05-24 19:00:25 -0600

    Calculadora

---
+++ Parcia1/Calculadora/Calculadora/Form1.Designer.vb
@@ -0,0 +1,380 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label4 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(85, 9)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(220, 30)
+        Label4.TabIndex = 10
+        Label4.Text = "Calculadora Pablo SC"
+        Label4.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.WindowFrame
+        Button1.Font = New Font("Minecraft", 14.25F)
+        Button1.ForeColor = SystemColors.ControlLightLight
+        Button1.Location = New Point(118, 427)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(79, 63)
+        Button1.TabIndex = 11
+        Button1.Text = "0"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = SystemColors.WindowFrame
+        Button2.Font = New Font("Minecraft", 14.25F)
+        Button2.ForeColor = SystemColors.ControlLightLight
+        Button2.Location = New Point(33, 358)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(79, 63)
+        Button2.TabIndex = 12
+        Button2.Text = "1"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.WindowFrame
+        Button3.Font = New Font("Minecraft", 14.25F)
+        Button3.ForeColor = SystemColors.ControlLightLight
+        Button3.Location = New Point(118, 358)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(79, 63)
+        Button3.TabIndex = 13
+        Button3.Text = "2"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = SystemColors.WindowFrame
+        Button4.Font = New Font("Minecraft", 14.25F)
+        Button4.ForeColor = SystemColors.ControlLightLight
+        Button4.Location = New Point(203, 358)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(79, 63)
+        Button4.TabIndex = 14
+        Button4.Text = "3"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = SystemColors.WindowFrame
+        Button5.Font = New Font("Minecraft", 14.25F)
+        Button5.ForeColor = SystemColors.ControlLightLight
+        Button5.Location = New Point(33, 427)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(79, 63)
+        Button5.TabIndex = 15
+        Button5.Text = "."
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = SystemColors.WindowFrame
+        Button6.Font = New Font("Minecraft", 14.25F)
+        Button6.ForeColor = SystemColors.ControlLightLight
+        Button6.Location = New Point(203, 427)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(79, 63)
+        Button6.TabIndex = 16
+        Button6.Text = "="
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = SystemColors.WindowFrame
+        Button7.Font = New Font("Minecraft", 14.25F)
+        Button7.ForeColor = SystemColors.ControlLightLight
+        Button7.Location = New Point(288, 427)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(79, 63)
+        Button7.TabIndex = 17
+        Button7.Text = "+"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = SystemColors.WindowFrame
+        Button8.Font = New Font("Minecraft", 14.25F)
+        Button8.ForeColor = SystemColors.ControlLightLight
+        Button8.Location = New Point(33, 289)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(79, 63)
+        Button8.TabIndex = 18
+        Button8.Text = "4"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = SystemColors.WindowFrame
+        Button9.Font = New Font("Minecraft", 14.25F)
+        Button9.ForeColor = SystemColors.ControlLightLight
+        Button9.Location = New Point(118, 289)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(79, 63)
+        Button9.TabIndex = 19
+        Button9.Text = "5"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = SystemColors.WindowFrame
+        Button10.Font = New Font("Minecraft", 14.25F)
+        Button10.ForeColor = SystemColors.ControlLightLight
+        Button10.Location = New Point(203, 289)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(79, 63)
+        Button10.TabIndex = 20
+        Button10.Text = "6"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = SystemColors.WindowFrame
+        Button11.Font = New Font("Minecraft", 14.25F)
+        Button11.ForeColor = SystemColors.ControlLightLight
+        Button11.Location = New Point(288, 358)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(79, 63)
+        Button11.TabIndex = 21
+        Button11.Text = "-"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = SystemColors.WindowFrame
+        Button12.Font = New Font("Minecraft", 14.25F)
+        Button12.ForeColor = SystemColors.ControlLightLight
+        Button12.Location = New Point(33, 220)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(79, 63)
+        Button12.TabIndex = 22
+        Button12.Text = "7"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = SystemColors.WindowFrame
+        Button13.Font = New Font("Minecraft", 14.25F)
+        Button13.ForeColor = SystemColors.ControlLightLight
+        Button13.Location = New Point(118, 220)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(79, 63)
+        Button13.TabIndex = 23
+        Button13.Text = "8"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = SystemColors.WindowFrame
+        Button14.Font = New Font("Minecraft", 14.25F)
+        Button14.ForeColor = SystemColors.ControlLightLight
+        Button14.Location = New Point(203, 220)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(79, 63)
+        Button14.TabIndex = 24
+        Button14.Text = "9"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = SystemColors.WindowFrame
+        Button15.Font = New Font("Minecraft", 14.25F)
+        Button15.ForeColor = SystemColors.ControlLightLight
+        Button15.Location = New Point(288, 289)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(79, 63)
+        Button15.TabIndex = 25
+        Button15.Text = "x"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = SystemColors.WindowFrame
+        Button16.Font = New Font("Minecraft", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button16.ForeColor = SystemColors.ControlLightLight
+        Button16.Location = New Point(288, 220)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(79, 63)
+        Button16.TabIndex = 26
+        Button16.Text = "/"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = SystemColors.WindowFrame
+        Button17.Font = New Font("Minecraft", 14.25F)
+        Button17.ForeColor = SystemColors.ControlLightLight
+        Button17.Location = New Point(203, 151)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(79, 63)
+        Button17.TabIndex = 27
+        Button17.Text = "<X"
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = SystemColors.WindowFrame
+        Button18.Font = New Font("Minecraft", 14.25F)
+        Button18.ForeColor = SystemColors.ControlLightLight
+        Button18.Location = New Point(118, 151)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(79, 63)
+        Button18.TabIndex = 28
+        Button18.Text = "C"
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Button19
+        ' 
+        Button19.BackColor = SystemColors.WindowFrame
+        Button19.Font = New Font("Minecraft", 14.25F)
+        Button19.ForeColor = SystemColors.ControlLightLight
+        Button19.Location = New Point(33, 151)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(79, 63)
+        Button19.TabIndex = 29
+        Button19.Text = "CE"
+        Button19.UseVisualStyleBackColor = False
+        ' 
+        ' Button20
+        ' 
+        Button20.BackColor = SystemColors.WindowFrame
+        Button20.Font = New Font("Minecraft", 14.25F)
+        Button20.ForeColor = SystemColors.ControlLightLight
+        Button20.Location = New Point(288, 151)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(79, 63)
+        Button20.TabIndex = 30
+        Button20.Text = "%"
+        Button20.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Minecraft", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox1.Location = New Point(33, 98)
+        TextBox1.Name = "TextBox1"
+        TextBox1.ReadOnly = True
+        TextBox1.Size = New Size(334, 34)
+        TextBox1.TabIndex = 31
+        TextBox1.Text = "0"
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Minecraft", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        TextBox2.ForeColor = SystemColors.WindowFrame
+        TextBox2.Location = New Point(33, 58)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(334, 27)
+        TextBox2.TabIndex = 32
+        TextBox2.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(402, 515)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button20)
+        Controls.Add(Button19)
+        Controls.Add(Button18)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label4)
+        Name = "Form1"
+        Text = "Calculadora V 1.0"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+
+End Class
---
+++ Parcia1/Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,275 @@
+﻿Imports System.Data
+
+Public Class Form1
+
+    '=================================================
+    ' Configuración inicial
+    '=================================================
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Permite detectar teclas del teclado
+        Me.KeyPreview = True
+
+        'Evita escritura manual
+        TextBox1.ReadOnly = True
+        TextBox2.ReadOnly = True
+
+        'Alinea texto a la derecha
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        TextBox2.TextAlign = HorizontalAlignment.Right
+
+    End Sub
+
+
+    '=================================================
+    ' Agregar números y operadores
+    ' Evita operadores duplicados
+    '=================================================
+    Private Sub Agregar(valor As String)
+
+        Dim operadores As String = "+-*/"
+
+        If TextBox2.Text.Length > 0 Then
+
+            Dim ultimo As String =
+            TextBox2.Text.Substring(TextBox2.Text.Length - 1)
+
+            'Evita ++ -- **/
+            If operadores.Contains(ultimo) And operadores.Contains(valor) Then
+                Exit Sub
+            End If
+
+        End If
+
+        TextBox2.Text &= valor
+
+    End Sub
+
+
+    '=================================================
+    ' BOTONES NUMÉRICOS
+    '=================================================
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Agregar("0")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Agregar("1")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Agregar("2")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Agregar("3")
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Agregar("4")
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Agregar("5")
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Agregar("6")
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Agregar("7")
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Agregar("8")
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Agregar("9")
+    End Sub
+
+
+    '=================================================
+    ' Punto decimal
+    ' Evita varios puntos en un número
+    '=================================================
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim partes() As String =
+        TextBox2.Text.Split("+"c, "-"c, "*"c, "/"c)
+
+        Dim ultimoNumero As String =
+        partes(partes.Length - 1)
+
+        If Not ultimoNumero.Contains(".") Then
+            Agregar(".")
+        End If
+
+    End Sub
+
+
+    '=================================================
+    ' OPERADORES
+    '=================================================
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Agregar("+")
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Agregar("-")
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Agregar("*")
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Agregar("/")
+    End Sub
+
+
+    '=================================================
+    ' Porcentaje
+    '=================================================
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+
+        Try
+
+            Dim valor As Double
+
+            valor = Convert.ToDouble(TextBox2.Text)
+
+            valor = valor / 100
+
+            Dim resultadoFinal As String =
+            valor.ToString("0.######")
+
+            TextBox1.Text = resultadoFinal
+            TextBox2.Text = resultadoFinal
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error")
+
+        End Try
+
+    End Sub
+
+
+    '=================================================
+    ' Igual
+    ' Calcula y limita decimales mostrados
+    '=================================================
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        Try
+
+            Dim resultado As Object
+
+            resultado = New DataTable().Compute(TextBox2.Text, Nothing)
+
+            'Convierte a número decimal
+            Dim valor As Double = Convert.ToDouble(resultado)
+
+            'Muestra máximo 6 decimales y elimina ceros sobrantes
+            Dim resultadoFinal As String =
+            valor.ToString("0.######")
+
+            'Actualiza resultados
+            TextBox1.Text = resultadoFinal
+            TextBox2.Text = resultadoFinal
+
+        Catch ex As Exception
+
+            MessageBox.Show("Operación inválida")
+
+        End Try
+
+    End Sub
+
+
+    '=================================================
+    ' CE
+    ' Borra solo operación actual
+    '=================================================
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+
+        TextBox2.Clear()
+
+    End Sub
+
+
+    '=================================================
+    ' C
+    ' Borra toda la calculadora
+    '=================================================
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+
+    End Sub
+
+
+    '=================================================
+    ' <x
+    ' Borra un carácter
+    '=================================================
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+
+        If TextBox2.Text.Length > 0 Then
+
+            TextBox2.Text =
+            TextBox2.Text.Substring(0,
+            TextBox2.Text.Length - 1)
+
+        End If
+
+    End Sub
+
+
+    '=================================================
+    ' Soporte para teclado
+    '=================================================
+
+    Private Sub Form1_KeyPress(sender As Object,
+    e As KeyPressEventArgs) Handles Me.KeyPress
+
+        If Char.IsDigit(e.KeyChar) Then
+            Agregar(e.KeyChar)
+        End If
+
+        Select Case e.KeyChar
+
+            Case "+"
+                Agregar("+")
+
+            Case "-"
+                Agregar("-")
+
+            Case "*"
+                Agregar("*")
+
+            Case "/"
+                Agregar("/")
+
+            Case "."
+                Agregar(".")
+
+            Case Chr(13)
+                Button6.PerformClick()
+
+        End Select
+
+    End Sub
+
+End Class
